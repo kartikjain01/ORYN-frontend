90f55988e5bcf35be65fcb12a5b3d131b043e871
Kartik <kartik@Kartiks-MacBook-Air.local>
2026-04-10 11:42:21 +0530

    Updated register page UI (removed full name input, kept metadata)

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -242,7 +242,7 @@ export default function AuthPage() {
             </p>
           </div>
 
-          <div className="relative">
+          {/*<div className="relative">
             <User className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
             <input
               type="text"
@@ -251,7 +251,7 @@ export default function AuthPage() {
               onChange={e => setFullName(e.target.value)}
               className="w-full pl-12 pr-4 py-3 rounded-xl bg-gray-50 border border-gray-200 text-black placeholder-gray-400 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 outline-none transition"
             />
-          </div>
+          </div>*/}
 
           <div className="space-y-5">
             {/* Email */}
